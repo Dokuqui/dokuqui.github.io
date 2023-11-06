cfb015c757737190db2668526763c9687d0eb6cc
Illia <136450403+Dokuqui@users.noreply.github.com>
2023-11-06 12:13:15 +0000

    Button Mode

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.scrolling-button');
+  });
